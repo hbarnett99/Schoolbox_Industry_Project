f7cbe7986691375a11ebbeb2be58350a1b0ce24b
datenyan <13450157+Datenyan@users.noreply.github.com>
2021-08-26 11:43:48 +1000

    Cleaned up footer

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -2,34 +2,33 @@
 
 ?>
 
-<footer class="footer pt-5  ">
+<footer class="footer py-3">
 <div class="container-fluid">
     <div class="row align-items-center justify-content-lg-between">
-    <div class="col-lg-6 mb-lg-0 mb-4">
-        <div class="copyright text-center text-sm text-muted text-lg-start">
-        © <script>
-            document.write(new Date().getFullYear())
-        </script>,
-        Created by Marble Benchtop
+        <div class="col-lg-6 mb-lg-0 mb-4">
+            <div class="copyright text-center text-sm text-muted text-lg-start">
+        ©   <script>
+                document.write(new Date().getFullYear())
+            </script>,
+            Created by Marble Benchtop | Data by Schoolbox
+            </div>
         </div>
-    </div>
-    <div class="col-lg-6">
-        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-        <li class="nav-item">
-            <?= $this->Html->link(
-                'All Datasets',
-                ['controller' => 'HistoricalFacts', 'action' => 'index'], array('class' => 'nav-link text-muted')
-            ) ?>
-        </li>
-            <li class="nav-item">
-                <?= $this->Html->link(
-                    'Latest Dataset',
-                    ['controller' => 'HistoricalFacts', 'action' => 'view', '5'], array('class' => 'nav-link text-muted')
-                ) ?>        </li>
-        </ul>
-    </div>
-        NOTE - LATEST DATASET IS HARDCODED, VARIABLE NEEDS TO BE IMPLEMENTED
-
+        <div class="col-lg-6">
+            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                <li class="nav-item">
+                    <?= $this->Html->link(
+                        'All Datasets',
+                        ['controller' => 'Historical-Facts', 'action' => 'index'], array('class' => 'nav-link text-muted')
+                    ) ?>
+                </li>
+                <li class="nav-item">
+                    <?= $this->Html->link(
+                        'Latest Dataset',
+                        ['controller' => 'Historical-Facts', 'action' => 'newest-data'], array('class' => 'nav-link text-muted')
+                    ) ?>
+                </li>
+            </ul>
+        </div>
     </div>
 </div>
 </footer>
