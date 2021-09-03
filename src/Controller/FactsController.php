b6cd3bb2a6ce5114dab20b7cee244008fe15eb2e
datenyan <13450157+Datenyan@users.noreply.github.com>
2021-09-03 20:30:49 +1000

    Added support for deeplink query strings

--- src/Controller/FactsController.php
+++ src/Controller/FactsController.php
@@ -23,10 +23,18 @@ class FactsController extends AppController
     public function beforeFilter(EventInterface $event)
     {
         parent::beforeFilter($event);
+
+        // Capture query params if passed
+        $queryString = '?';
+        foreach ($this->request->getQueryParams() as $key => $value) {
+            $queryString .= $key . '=' . $value . '&';
+        }
+        $queryString = Substr_replace($queryString, "", -1);
+
         $path = $this->request->getPath();
         $userEmail = $this->request->getSession()->read('Auth.email');
         if ($userEmail == null) {
-            $this->redirect('/users/login?redirect=' . $path);
+            $this->redirect('/users/login?redirect=' . $path . $queryString);
         }
     }
 
--- src/Controller/HistoricalFactsController.php
+++ src/Controller/HistoricalFactsController.php
@@ -19,10 +19,18 @@ class HistoricalFactsController extends AppController
     {
         parent::beforeFilter($event);
         $this->loadModel('HistoricalFacts');
+        
+        // Capture query params if passed
+        $queryString = '?';
+        foreach ($this->request->getQueryParams() as $key => $value) {
+            $queryString .= $key . '=' . $value . '&';
+        }
+        $queryString = Substr_replace($queryString, "", -1);
+
         $path = $this->request->getPath();
         $userEmail = $this->request->getSession()->read('Auth.email');
         if ($userEmail == null) {
-            $this->redirect('/users/login?redirect=' . $path);
+            $this->redirect('/users/login?redirect=' . $path . $queryString);
         }
     }
 
