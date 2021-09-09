4b26b25c75de34789aae0be062b8f604b3d70f1d
datenyan <13450157+Datenyan@users.noreply.github.com>
2021-09-09 19:27:38 +1000

    Removed unnecessary link on logo

--- templates/element/navbar.php
+++ templates/element/navbar.php
@@ -6,7 +6,7 @@
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="#">
+      <a class="navbar-brand m-0">
         <?= $this->Html->image('../favicon.ico', ['alt' => 'SchoolBox_Logo']) ?>
         <span class="ms-1 font-weight-bold">Schoolbox Dashboard</span>
       </a>
