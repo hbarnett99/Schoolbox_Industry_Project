2f005b45f63795b688f7caa5aafc1f3e9330a3db
datenyan <13450157+Datenyan@users.noreply.github.com>
2021-09-23 22:30:32 +1000

    Added HTML title

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -1,8 +1,7 @@
 <?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\User[]|\Cake\Collection\CollectionInterface $users
- */
+
+$this->assign('title', 'Manage Users');
+
 ?>
 <div class="row">
     <div class="col-12">
